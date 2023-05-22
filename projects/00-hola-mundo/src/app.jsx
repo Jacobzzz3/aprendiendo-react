21ae6bd786d0741a13b926e16dc491ad5ee84709
Jacobzzz3 <carlosguerrerogarcia2004@gmail.com>
2023-05-22 14:14:18 -0400

    First Projects

---
+++ projects/00-hola-mundo/src/app.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+export function App () {
+
+    const format = (userName) => `@${userName}`
+
+    return(
+    <section className='app'>
+    <TwitterFollowCard formatUserName={format}  userName="jacob" initialIsFollowing={true}>
+        Carlos Guerrero  Garcia
+    </TwitterFollowCard>
+
+    <TwitterFollowCard formatUserName={format}  userName="carlosjj77679" >
+        fedelkaga2
+    </TwitterFollowCard>
+    
+
+
+    
+    </section>
+
+
+    )
+}
